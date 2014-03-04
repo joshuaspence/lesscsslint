c3f019be4f4a65c4846d2c3682214869986f6053
Joshua Spence <josh@joshuaspence.com>
2014-03-05 09:55:46 +1100

    Made code more modular.

--- bin/lesscsslint.js
+++ bin/lesscsslint.js
@@ -3,84 +3,10 @@
 'use strict';
 
 var _ = require('lodash'),
-    program = require('commander'),
-    csslint = require('csslint').CSSLint,
+    csslint = require('../lib/csslint'),
     fs = require('fs'),
-    less = require('less'),
     path = require('path'),
-    SourceMapConsumer = require('source-map').SourceMapConsumer,
-    util = require('../lib/util');
-
-var lessOptions = {
-    compress: false,
-    cleancss: false,
-    optimization: null,
-    paths: []
-};
-
-var parseLessFile = function(input, data) {
-    if (input) {
-        lessOptions.paths = [path.dirname(input)].concat(lessOptions.paths);
-    }
-    lessOptions.filename = input;
-
-    var smc;
-
-    var parser = new(less.Parser)(lessOptions);
-    parser.parse(data, function(err, tree) {
-        if (err) {
-            console.error(err);
-            return;
-        } else {
-            var css = tree.toCSS(_.extend(lessOptions, {
-                sourceMap: true,
-                writeSourceMap: function(sourceMap) {
-                    smc = new SourceMapConsumer(sourceMap);
-                }
-            }));
-
-            var ruleset = csslint.getRuleset();
-            csslint.getRules().forEach(function(rule) {
-                if (_.contains(program.errors, rule)) {
-                    ruleset[rule.id] = 2;
-                } else if (_.contains(program.warnings, rule)) {
-                    ruleset[rule.id] = 1;
-                } else if (_.contains(program.ignore, rule)) {
-                    ruleset[rule.id] = 0;
-                }
-            });
-
-            results = csslint.verify(css, ruleset);
-
-            results.messages = _(results.messages).filter(function(message) {
-                var position = smc.originalPositionFor({
-                    line: message.line,
-                    column: message.col
-                });
-
-                if (position.source !== (input || 'input')) {
-                    return false;
-                }
-
-                message.line = position.line;
-                message.column = position.column;
-                return true;
-            });
-
-            var formatter = csslint.getFormatter(program.format);
-            console.log(formatter.formatResults(results, input || 'STDIN'));
-        }
-    });
-};
-
-program
-    .version(require('../package.json').version)
-    .usage('[options] <files>')
-    .option('--format <format>', 'Indicate which format to use for output.', 'text')
-    .option('--errors <rules>', 'Indicate which CSSLint rules to include as errors.', util.list)
-    .option('--warnings <rules>', 'Indicate which rules to include as warnings.', util.list)
-    .option('--ignore <rules>', 'Indicate which rules to ignore completely.', util.list)
-    .parse(process.argv);
+    program = require('../lib/cli').parse(process.argv);
 
 if (program.args.length === 0) {
     program.outputHelp();
@@ -96,7 +22,7 @@ _(program.args).forEach(function(input) {
                 return;
             }
 
-            parseLessFile(input, data);
+            csslint.verifyLess(input, data);
         });
     } else {
         process.stdin.resume();
@@ -108,7 +34,7 @@ _(program.args).forEach(function(input) {
         });
 
         process.stdin.on('end', function() {
-            parseLessFile(null, buffer);
+            csslint.verifyLess(null, buffer);
         });
     }
 });
