36f8a25a0c38d4ca6c472dd0772d604a5939d488
Joshua Spence <josh@joshuaspence.com>
2014-03-05 16:09:30 +1100

    Minor formatting changes.

--- test/csslint.js
+++ test/csslint.js
@@ -22,8 +22,10 @@ module.exports = (function() {
 
             fs.readFile(file, 'utf8', function(err, data) {
                 test.ifError(err);
+
                 csslint.verify(file, data, csslint.getRuleset(), function(err, results) {
                     test.ifError(err);
+
                     fs.readFile(path.join('test/less', basename + '.txt'), 'utf8', function(err, expectedResults) {
                         test.ifError(err);
                         test.equal(formatter.formatResults(results, file), expectedResults);
