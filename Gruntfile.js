969beda31252917c253878310a2890d831b12fa6
Joshua Spence <josh@joshuaspence.com>
2014-03-05 18:46:26 +1100

    Added additional JavaScript linting with `jscs`.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,15 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        jscs: {
+            options: {
+                config: '.jscsrc'
+            },
+            bin: 'bin/**/*.js',
+            grunt: 'Gruntfile.js',
+            lib: 'lib/**/*.js',
+            test: '<%= nodeunit.all %>'
+        },
         jshint: {
             options: {
                 jshintrc: true
@@ -27,12 +36,13 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs-checker');
     grunt.loadNpmTasks('grunt-jsonlint');
     grunt.loadNpmTasks('grunt-contrib-nodeunit');
     grunt.loadNpmTasks('grunt-release');
 
     // Register tasks.
-    grunt.registerTask('lint', ['jsonlint', 'jshint']);
+    grunt.registerTask('lint', ['jsonlint', 'jshint', 'jscs']);
     grunt.registerTask('test', ['nodeunit']);
     grunt.registerTask('default', ['lint', 'test']);
 };
