5f010facb3e326b78b617b485ad0b14beb383b76
Joshua Spence <josh@joshuaspence.com>
2014-03-05 23:56:55 +1100

    Added a test for the main binary script.

---
+++ test/lesscsslint.js
@@ -0,0 +1,53 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+
+module.exports.errorCodes = {
+    fileNotExist: function(test) {
+        var lesscsslint = spawn('bin/lesscsslint.js', ['test/less/foo.bar']);
+
+        test.expect(1);
+        lesscsslint.on('close', function(code) {
+            test.notEqual(code, 0);
+            test.done();
+        });
+    },
+    invalidCss: function(test) {
+        var lesscsslint = spawn('bin/lesscsslint.js', ['test/less/css.less']);
+
+        test.expect(1);
+        lesscsslint.on('close', function(code) {
+            test.notEqual(code, 0);
+            test.done();
+        });
+    },
+    invalidLess: function(test) {
+        var lesscsslint = spawn('bin/lesscsslint.js', ['-']);
+
+        test.expect(1);
+        lesscsslint.stdin.write('.foo {');
+        lesscsslint.stdin.end()
+        lesscsslint.on('close', function(code) {
+            test.notEqual(code, 0);
+            test.done();
+        });
+    },
+    ok: function(test) {
+        var lesscsslint = spawn('bin/lesscsslint.js', ['test/less/ok.less']);
+
+        test.expect(1);
+        lesscsslint.on('close', function(code) {
+            test.equal(code, 0);
+            test.done();
+        });
+    },
+    usage: function(test) {
+        var lesscsslint = spawn('bin/lesscsslint.js', []);
+
+        test.expect(1);
+        lesscsslint.on('close', function(code) {
+            test.notEqual(code, 0);
+            test.done();
+        });
+    }
+};
